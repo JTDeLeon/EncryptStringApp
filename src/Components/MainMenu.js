786edbf67d6b9b97d14018b1c233a39f407432f7
Jonathan Deleon <Jondeleon856@gmail.com>
2018-05-10 01:02:34 -0400

    added functionality of exit button

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -10,6 +10,33 @@ class MainMenu extends Component {
     this.props.isClicked(true);
   }
 
+
+  handleExit = (e) => {
+    e.preventDefault();
+    console.log("exit selected");
+
+    const target = document.querySelector('.App');
+
+    target.innerHTML = " ";
+
+    const goodbyeMessage = document.createElement('h1');
+    goodbyeMessage.textContent = "Thanks for encrypting, see you next time you want to be secure!";
+
+    target.appendChild(goodbyeMessage);
+
+    const refreshButton = document.createElement('button');
+
+    refreshButton.textContent = "Didn't mean to exit? Start Over";
+    refreshButton.setAttribute('href',"#");
+    refreshButton.setAttribute('id',"refresh-btn");
+    refreshButton.setAttribute('onClick',"window.location.reload()");
+
+    target.appendChild(refreshButton);
+
+
+  }
+
+
   render() {
     return (
     <div className="main-menu-container">
@@ -22,7 +49,7 @@ class MainMenu extends Component {
       </button>
 
       {/*Exits the user from program with a nice message */}
-      <button>
+      <button onClick={this.handleExit}>
         Exit
       </button>
     </div>
